cc8a5098e7279ad3e173f2ea257ff44aff8ea39e
Known <108655320+knana6@users.noreply.github.com>
2025-04-22 21:01:06 +0900

    Create 4_22.py

---
+++ 4_22.py
@@ -0,0 +1,16 @@
+#0. split()의 반대 join()
+w=['hello''world']
+usejoin=''.join(w)
+print(usejoin)  #hello vworld
+ww='apple benana'
+usesplit=ww.split(',')
+print(usespllit) #['apple','banna']
+
+#list, tuple, dict
+#1. 입력한 값을 리스트로 생성
+num=[0,0,0]
+num[0]=int(input('첫번째 삽입할 숫자'))
+listprint=num[0]+num[1]+num[2]
+print(listprint)
+
+#2. 반복문 활용 리스트 추가- list.append()
